40c186f0f5f3726db813320012a27522155e7e0c
yosuke0517 <yosuketakeuchi.dev@gmail.com>
2022-04-05 01:52:36 +0900

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,30 @@
+<script setup>
+// import CKEditor from "@ckeditor/ckeditor5-vue";
+import BalloonEditor from "@ckeditor/ckeditor5-build-balloon";
+// import { ref } from "vue";
+import { computed } from "@vue/reactivity";
+import { useStore } from "@/store";
+import { ADD_COMPONENT } from "@/store/mutationTypes";
+const store = useStore();
+const components = computed(() => store.state.components);
+const addComponent = (component) => {
+  store.commit(ADD_COMPONENT, {
+    components: component,
+  });
+};
+const editor = BalloonEditor;
+// const editorData = ref("<p>Content of the editor.</p>");
+const editorConfig = {};
+</script>
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="about">
+    <h1>CKEditor TEST（js）</h1>
+    <ckeditor
+      v-for="(component, i) in components"
+      :key="i"
+      :editor="editor"
+      :config="editorConfig"
+    ></ckeditor>
+    <button type="button" @click="addComponent('component')">add Editor</button>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
